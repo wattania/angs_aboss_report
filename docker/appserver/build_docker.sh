41507f4b414dca4dc2e21bcbf6909789ec4461d8
trinity-dev <wattaint@gmail.com>
2015-08-19 00:21:31 -0400

    add rails

---
+++ docker/appserver/build_docker.sh
@@ -0,0 +1,1 @@
+docker build -t docker.io/angstroms/angs_aboss_report:appserver .
---
+++ docker/appserver/run_docker_dev.sh
@@ -0,0 +1,4 @@
+docker run --rm -it \
+-v $(pwd)/../..:/backup \
+docker.io/angstroms/angs_aboss_report:appserver \
+bash
