ddc59282dc3d4300485dd22e626f8c0658c4ff16
Wattana Inthaphong <wattaint@gmail.com>
2015-08-24 16:28:55 +0700

    test get info from oauth

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -2,6 +2,7 @@ docker run --rm -it \
 -p 3000:3000 \
 -v $(pwd)/appserver:/appserver \
 --link angs_aboss_report_postgresql:pg_server \
+--link mad_goldstine:auth.wattaint.net \
 --privileged \
 docker.io/angstroms/angs_aboss_report:latest \
 bash
