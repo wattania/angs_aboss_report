aa06ebb6b78dd49caac9859c4c54dea8567e2ffb
trinity-dev <wattaint@gmail.com>
2015-08-26 02:24:08 -0400

    test rails 3.2.22

--- appserver/app/controllers/application_controller.rb
+++ appserver/app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
-  def authentication_callback
-    auth = request.env['omniauth.auth']
-    render json: auth.to_json
-  end
+  protect_from_forgery
 end
---
+++ appserver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appserver::Application.config.secret_token = '37e3781603ca62a7eb05e912e57df52f279bc29974f991e3f7aa1e161854fe9df729eff87ba18aded5325f83f876b719cfc2212c8b02593b4ffd12702c795d15'
