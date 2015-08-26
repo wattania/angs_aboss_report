2e51d0a84ac4fe4b71d6688683a28fa8ae043518
Wattana Inthaphong <wattaint@gmail.com>
2015-08-26 15:09:26 +0700

    x

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -1,6 +1,7 @@
 docker run --rm -it \
---hostname=angs_aboss_report \
+--hostname=docker_angs_aboss_report \
 -p 3000:3000 \
+-e JRUBY_OPTS="-J-Xmx2g -J-Xms1g -J-Xss8192k" \
 -v $(pwd)/appserver:/appserver \
 --link angs_aboss_report_postgresql:pg_server \
 --link evil_hoover:auth.wattaint.net \
