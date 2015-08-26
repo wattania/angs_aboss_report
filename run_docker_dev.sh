9c6b09881b2032fd4c951f3276fe0070ea4b87bf
trinity-dev <wattaint@gmail.com>
2015-08-26 04:14:33 -0400

    x

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -1,10 +1,9 @@
 docker run --rm -it \
 --hostname=docker_angs_aboss_report \
--p 3000:3000 \
+-p 3005:3000 \
 -e JRUBY_OPTS="-J-Xmx2g -J-Xms1g -J-Xss8192k" \
 -v $(pwd)/appserver:/appserver \
 --link angs_aboss_report_postgresql:pg_server \
---link evil_hoover:auth.wattaint.net \
 --privileged \
 docker.io/angstroms/angs_aboss_report:latest \
 bash
