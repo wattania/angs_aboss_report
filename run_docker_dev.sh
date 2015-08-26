1a30ac621d46566dc7d5f8907ad821fb5098e1fc
Wattana Inthaphong <wattaint@gmail.com>
2015-08-26 15:34:23 +0700

    xx

--- run_docker_dev.sh
+++ run_docker_dev.sh
@@ -1,8 +1,9 @@
 docker run --rm -it \
 --hostname=docker_angs_aboss_report \
 -p 3005:3000 \
--e JRUBY_OPTS="-J-Xmx2g -J-Xms1g -J-Xss8192k" \
+-e JRUBY_OPTS="-J-Xmx2g -J-Xms1g" \
 -v $(pwd)/appserver:/appserver \
+--link aboss_auth_dev:auth.wattaint.net \
 --link angs_aboss_report_postgresql:pg_server \
 --privileged \
 docker.io/angstroms/angs_aboss_report:latest \
